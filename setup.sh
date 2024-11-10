998a437b1fecf20f5715908eb9ed82d59bd217b5
Raj Ramalingam <rajramalingam@rajs-mbp.lan>
2024-11-10 10:01:21 -0800

    Updated setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 kind create cluster --name spark-cluster
 helm repo add spark-operator https://kubeflow.github.io/spark-operator
 helm repo update
+# Install Spark Operator
 helm install spark-operator spark-operator/spark-operator \
-    --namespace spark-operator \
-    --create-namespace
+    --namespace spark \
+    --create-namespace \
+    --set sparkJobNamespace=spark
